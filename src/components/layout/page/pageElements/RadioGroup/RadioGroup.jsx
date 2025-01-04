df4aa535277d1319bf0411f81891f9d53058b1a0
Grigory Perevalov <grigoryperevalov@Grigorys-MacBook-Pro.local>
2025-01-05 03:23:45 +0500

    [fix] fix styles and add some features

--- src/components/layout/page/pageElements/RadioGroup/RadioGroup.jsx
+++ src/components/layout/page/pageElements/RadioGroup/RadioGroup.jsx
@@ -11,19 +11,20 @@ export const RadioGroup = ({ onChange, currentValue, radios }) => {
 
     return (
         <div className="radio-group">
-            {radios.map(({ value, icon }) => (
+            {radios.map(({ value, icon, isDisabled = false }) => (
                 <div key={value} className="radio-group__item">
                     <input
                         type="radio"
                         id={value}
                         value={value}
                         checked={currentValue === value}
+                        disabled={isDisabled}
                         onChange={handleChange}
                     />
-                    <label className="radio-group__label" htmlFor={value}>
-                        <div className="custom-radio"></div>
+                    <label className="radio-group__label" style={{ ...(isDisabled && { cursor: 'not-allowed' })}} htmlFor={value}>
+                        <div className="custom-radio" style={{ ...(isDisabled && { borderColor: '#888' })}}></div>
                         <div className="radio-group__logo">
-                            <span><img src={icon}/></span>
+                            <span><img style={{ ...(isDisabled && { filter: 'grayscale(100%)' })}} src={icon}/></span>
                         </div>
                     </label>
                 </div>
@@ -36,6 +37,7 @@ RadioGroup.propTypes = {
     radios: PropTypes.arrayOf(PropTypes.shape({
         value: PropTypes.string.isRequired,
         icon: PropTypes.string.isRequired,
+        isDisabled: PropTypes.bool,
     })).isRequired,
     onChange: PropTypes.func.isRequired,
     currentValue: PropTypes.string.isRequired,
