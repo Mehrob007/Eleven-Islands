13fe9872cfdafbb9aac1f8cb2e920680fdeacfe1
Mehrob <your_email@example.com>
2025-01-05 05:22:07 +0500

    edit url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://backendeleven.ru',
+  baseURL: 'https://backendeleven.ru/',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   }
